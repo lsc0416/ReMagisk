f0f1205d83d3be68d777085d686f9f7e38c7dbe3
Qiherko <xiaobai7891@qq.com>
2026-06-09 15:05:59 +0800

    更新 build.gradle.kts

--- app/apk/build.gradle.kts
+++ app/apk/build.gradle.kts
@@ -56,7 +56,7 @@ dependencies {
     implementation(libs.fragment.ktx)
     implementation(libs.appcompat)
     implementation(libs.material)
-implementation("com.github.Juky-App:SquircleView:1.0.1")
+    implementation(libs.squircleview)
 
     // Make sure kapt runs with a proper kotlin-stdlib
     kapt(kotlin("stdlib"))
